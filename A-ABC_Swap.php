88fac1e5326eca002ed1a872e6a10ae371b39e07
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-04-22 21:52:31 +0900

    add A-ABC_Swap/A-Coffee

---
+++ A-ABC_Swap.php
@@ -0,0 +1,3 @@
+<?php
+fscanf(STDIN, "%d %d %d", $X, $Y, $Z);
+echo($Z." ".$X." ".$Y).PHP_EOL;
---
+++ A-Coffee.php
@@ -0,0 +1,7 @@
+<?php
+fscanf(STDIN, "%s", $a);
+if ($a[2] == $a[3] && $a[4] == $a[5]) {
+    echo("Yes").PHP_EOL;
+} else {
+    echo("No").PHP_EOL;
+}
