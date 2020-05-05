0b651bc4be8d4095f8a567d53bf2d27cadb1f7e4
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-05-05 11:15:05 +0900

    A-A?C A-Dodecagon

---
+++ A-A?C.php
@@ -0,0 +1,7 @@
+<?php
+fscanf(STDIN, "%s", $S);
+if ($S === "ABC") {
+    echo("ARC").PHP_EOL;
+} elseif ($S === "ARC") {
+    echo("ABC").PHP_EOL;
+}
---
+++ A-Dodecagon.php
@@ -0,0 +1,4 @@
+<?php
+fscanf(STDIN, "%d", $r);
+$area = 3 * $r * $r;
+echo($area).PHP_EOL;
