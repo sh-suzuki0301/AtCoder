6ade7cdd888ab1870d272017b805b58204795fad
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-05-21 13:32:13 +0900

    add/A-Five_Antennas

---
+++ A-Five_Antennas.php
@@ -0,0 +1,8 @@
+<?php
+fscanf(STDIN, "%d", $a);
+fscanf(STDIN, "%d", $b);
+fscanf(STDIN, "%d", $c);
+fscanf(STDIN, "%d", $d);
+fscanf(STDIN, "%d", $e);
+fscanf(STDIN, "%d", $k);
+echo ($e - $a <= $k) ? 'Yay!' : ':(';
