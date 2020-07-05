4a4fb7a2c5687a7c185be3dc552854859740f89a
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-07-05 14:50:05 +0900

    A-Christmas_Eve_Eve_Eve

---
+++ A-Christmas_Eve_Eve_Eve.php
@@ -0,0 +1,17 @@
+<?php
+fscanf(STDIN, "%d", $d);
+
+switch ($d) {
+    case $d === 22:
+        echo 'Christmas Eve Eve Eve';
+        break;
+    case $d === 23:
+        echo 'Christmas Eve Eve';
+        break;
+    case $d === 24:
+        echo 'Christmas Eve';
+        break;
+    case $d === 25:
+        echo 'Christmas';
+        break;
+}
