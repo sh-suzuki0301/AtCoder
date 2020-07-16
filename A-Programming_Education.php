fcbf88b0ae65c79a4fe398f35bb7615c32672db4
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2020-07-16 22:00:00 +0900

    add/A-Programming_Education

---
+++ A-Programming_Education.php
@@ -0,0 +1,12 @@
+<?php
+fscanf(STDIN, "%d", $n);
+fscanf(STDIN, "%d", $a);
+fscanf(STDIN, "%d", $b);
+switch ($n) {
+    case $n === 1:
+        echo 'Hello World';
+        break;
+    case $n === 2:
+        echo $a + $b;
+        break;
+}
