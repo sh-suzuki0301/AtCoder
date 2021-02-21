4b104fffc79b6b170eb041eda2aac1affac2b1c9
sh-suzuki0301 <sh-suzuki@oz-vision.co.jp>
2021-02-21 10:33:01 +0900

    cosme

--- Changing_a_Character.php
+++ Changing_a_Character.php
@@ -3,8 +3,11 @@ fscanf(STDIN, '%d %d', $n, $k);
 fscanf(STDIN, '%s', $s);
  
 $ans = '';
-for($i = 0; $i < $n; $i++) {
-    if ($i === $k - 1) $ans = $ans . strtolower($s[$i]);
-    else $ans = $ans . $s[$i];
+for ($i = 0; $i < $n; $i++) {
+    if ($i === $k - 1) {
+        $ans = $ans . strtolower($s[$i]);
+    } else {
+        $ans = $ans . $s[$i];
+    }
 }
 echo $ans . PHP_EOL;
